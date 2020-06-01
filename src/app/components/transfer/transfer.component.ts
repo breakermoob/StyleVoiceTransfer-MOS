242ab77bb6bbfcf6829190dbdf72efba43a17b02
Breakermoob <breakermoob@hotmail.com>
2020-06-01 12:55:58 -0500

    Front y Back Conectados, blob to file -> Working

--- src/app/components/transfer/transfer.component.ts
+++ src/app/components/transfer/transfer.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as RecordRTC from 'recordrtc';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ApiModelService } from 'src/app/services/model/api-model.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -13,14 +13,15 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class TransferComponent implements OnInit {
 
 
+  public model = 0;
   public record;
   public recording = false;
   public url;
+  public file;
   public error;
   public modelResponse;
   public styleId;
   private stream: MediaStream;
-  form: FormGroup;
 
 
 
@@ -60,7 +61,7 @@ export class TransferComponent implements OnInit {
   stopRecording() {
     this.recording = false;
     console.log(this.record)
-    this.record.stop(this.processRecording.bind(this)); 
+    this.record.stop(this.processRecording.bind(this));
     let stream = this.stream;
     stream.getAudioTracks().forEach(track => track.stop());
   }
@@ -72,39 +73,36 @@ export class TransferComponent implements OnInit {
    */
   processRecording(blob) {
     this.url = URL.createObjectURL(blob);
+    let date = Date.now();
+    this.file = new File([blob], 'Record_' + date, { type: "audio/wav", lastModified: date });
   }
 
   errorCallback(error) {
     this.error = 'Can not play audio in your browser';
   }
-  saveAsBlob(){
-    //abrir el blob
-    // window.open(this.url);
-
+  
+  saveAsBlob() {
     //Descargar el blob con una etiqueta invisible machete :v
     let a = document.createElement('a');
-        document.body.appendChild(a);
-        a.setAttribute('style', 'display: none');
-        a.href = this.url;
-        a.download = "audio";
-        a.click();
-        // window.URL.revokeObjectURL(this.url);
-        a.remove();
+    document.body.appendChild(a);
+    a.setAttribute('style', 'display: none');
+    a.href = this.url;
+    a.download = "audio";
+    a.click();
+    a.remove();
   }
 
   processAudio() {
     if ((this.url) && (this.styleId)) {
       this.setModelResponse(this.url);
-      this.saveAsBlob();
+      // this.saveAsBlob();
 
       const formData = new FormData();
-      formData.append('target_id', this.styleId);
-      formData.append('model_id', "0");
-      formData.append('content_file', this.url);
+      formData.append('target_id', '2');
+      formData.append('model_id', '0');
+      formData.append('content_file', this.file);
 
-      // this.apiModel.trasnferOnFire(formData).subscribe((res) => {
-      //   console.log(res)
-      // });
+      this.apiModel.trasnferOnFire(formData);
     } else {
       alert('Debes grabar una frase y seleccionar un estilo')
     }
@@ -118,9 +116,6 @@ export class TransferComponent implements OnInit {
   setModelResponse(modelResponse) {
     this.modelResponse = modelResponse;
   }
-  // download() {
-  //   this.record.save('file.wav');
-  // }
 
   ngOnInit() {
   }
--- src/app/services/model/api-model.service.ts
+++ src/app/services/model/api-model.service.ts
@@ -8,19 +8,28 @@ import { Observable } from 'rxjs';
 })
 export class ApiModelService {
 
-  URL = 'http://35.231.122.36:8000';
+  URL = 'http://35.185.67.85:8000';
 
 
   constructor(private http: HttpClient) { }
 
   trasnferOnFire(data) {
-    const config = { headers: new HttpHeaders().set('Content-Type', 'application/json') };
-    return this.http.post(`${this.URL}/convert/`, data)
-      .pipe(map(res => {
-        //Procesar la respuesta 
-        console.log(res)
-        return res;
-      }));
+    // const config = { headers: new HttpHeaders().set('Accept', 'multipart/form-data') };
+    // return this.http.post(`${this.URL}/convert/`, data, config)
+    //   .subscribe(res => {
+    //     console.log(res)
+    //     return res;
+    //   });
+
+    this.http.post(this.URL + '/convert/', data).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error)
+    )
+    // .pipe(map(res => {
+    //   //Procesar la respuesta 
+    //   console.log(res)
+    //   return res;
+    // }));
   }
 
   healtCheck() {
