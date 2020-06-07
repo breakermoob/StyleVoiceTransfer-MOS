52ea6941561a2bd6051a26d81f58f7c035d59953
Breakermoob <breakermoob@hotmail.com>
2020-06-06 20:04:39 -0500

    List with queue and loader, examples and steps

--- src/app/components/transfer/transfer.component.ts
+++ src/app/components/transfer/transfer.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import * as RecordRTC from 'recordrtc';
-import { DomSanitizer } from '@angular/platform-browser';
-import { ApiModelService } from 'src/app/services/model/api-model.service';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import * as RecordRTC from "recordrtc";
+import { DomSanitizer } from "@angular/platform-browser";
+import { ApiModelService } from "src/app/services/model/api-model.service";
+import { FormBuilder } from "@angular/forms";
 
 @Component({
-  selector: 'app-transfer',
-  templateUrl: './transfer.component.html',
-  styleUrls: ['./transfer.component.css']
+  selector: "app-transfer",
+  templateUrl: "./transfer.component.html",
+  styleUrls: ["./transfer.component.css"],
 })
 export class TransferComponent implements OnInit {
-
-
   public model = 0;
   public record;
   public recording = false;
@@ -24,16 +22,22 @@ export class TransferComponent implements OnInit {
   private stream: MediaStream;
   public modelList;
   firstTransfer = false;
+  name: String = "";
+  listCount: number = 0;
+  queue: number = 0;
+  processing: boolean = false;
 
-
-
-  constructor(public fb: FormBuilder, private domSanitizer: DomSanitizer, private apiModel: ApiModelService) {
+  constructor(
+    public fb: FormBuilder,
+    private domSanitizer: DomSanitizer,
+    private apiModel: ApiModelService
+  ) {
     this.apiModel.healtCheck();
   }
 
   sanitize(url: string) {
     let audio = this.domSanitizer.bypassSecurityTrustUrl(url);
-    return audio
+    return audio;
   }
 
   initiateRecording() {
@@ -41,7 +45,8 @@ export class TransferComponent implements OnInit {
     this.recording = true;
     let mediaConstraints = {
       video: false,
-      audio: true
+      audio: true,
+      timeSlice: 1000,
     };
     navigator.mediaDevices
       .getUserMedia(mediaConstraints)
@@ -51,7 +56,7 @@ export class TransferComponent implements OnInit {
   successCallback(stream) {
     var options = {
       mimeType: "audio/wav",
-      numberOfAudioChannels: 1
+      numberOfAudioChannels: 1,
     };
     this.stream = stream;
     //Start Actuall Recording
@@ -64,9 +69,8 @@ export class TransferComponent implements OnInit {
     this.recording = false;
     this.record.stop(this.processRecording.bind(this));
     let stream = this.stream;
-    stream.getAudioTracks().forEach(track => track.stop());
+    stream.getAudioTracks().forEach((track) => track.stop());
   }
-
 
   /**
    * processRecording Do what ever you want with blob
@@ -75,73 +79,93 @@ export class TransferComponent implements OnInit {
   processRecording(blob) {
     this.url = URL.createObjectURL(blob);
     let date = Date.now();
-    this.file = new File([blob], 'Record_' + date, { type: "audio/wav", lastModified: date });
+    this.file = new File([blob], "Record_" + date, {
+      type: "audio/wav",
+      lastModified: date,
+    });
   }
 
   errorCallback(error) {
-    this.error = 'Can not play audio in your browser';
+    this.error = "Can not play audio in your browser";
   }
-
 
   processAudio() {
     this.firstTransfer = true;
-    if ((this.url) && (this.styleId)) {
+    if (this.url && this.styleId) {
+      this.listCount++;
+      if (this.modelList) {
+        this.queue = this.listCount - this.modelList.length;
+      }
+      if (this.queue > 0) {
+        this.processing = true;
+      } else {
+        this.processing = false;
+      }
+      this.name = this.getNameFromId(this.styleId);
       const formData = new FormData();
-      formData.append('target_id', this.styleId);
-      formData.append('model_id', '0');
-      formData.append('content_file', this.file);
+      formData.append("target_id", this.styleId);
+      formData.append("model_id", "0");
+      formData.append("content_file", this.file);
 
       this.apiModel.trasnferOnFire(formData).subscribe((response) => {
-        this.setModelResponse(response['content_url'], response['target_url']);
+        this.setModelResponse(
+          response["content_url"],
+          response["target_url"],
+          this.getNameFromId(this.styleId)
+        );
 
         if (!this.modelList) this.modelList = [];
 
         this.modelList.push({
           name: this.getNameFromId(this.styleId),
-          content_url: response['content_url'],
-          target_url: response['target_url']
-        })
-        console.log(this.modelList)
+          content_url: response["content_url"],
+          target_url: response["target_url"],
+        });
+        if (this.modelList) {
+          this.queue = this.listCount - this.modelList.length;
+        }
+        if (this.queue > 0) {
+          this.processing = true;
+        } else {
+          this.processing = false;
+        }
       });
-
     } else {
-      alert('Debes grabar una frase y seleccionar un estilo')
+      alert("Debes grabar una frase y seleccionar un estilo");
     }
   }
-
 
   setStyleId(id) {
     this.styleId = id;
   }
 
-  setModelResponse(content_url, target_url) {
+  setModelResponse(content_url, target_url, name) {
     this.target_url = target_url;
     this.content_url = content_url;
+    this.name = name;
   }
   getNameFromId(id) {
     switch (id) {
       case 1:
-        return "Jose Arango"
+        return "Jose Arango";
       case 2:
-        return "Carlos Montoya"
+        return "Carlos Montoya";
       case 3:
-        return "Leon Arango"
+        return "Leon Arango";
       default:
-        return "Invalid Id"
+        return "Invalid Id";
     }
   }
 
   saveAsBlob() {
-    let a = document.createElement('a');
+    let a = document.createElement("a");
     document.body.appendChild(a);
-    a.setAttribute('style', 'display: none');
+    a.setAttribute("style", "display: none");
     a.href = this.url;
     a.download = "audio";
     a.click();
     a.remove();
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/services/model/api-model.service.ts
+++ src/app/services/model/api-model.service.ts
@@ -1,28 +1,25 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { map } from "rxjs/operators";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ApiModelService {
+  URL = "http://34.74.168.60";
+  PORT = "8000";
 
-  URL = 'http://35.185.67.85';
-  PORT = '8000';
-
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   trasnferOnFire(data) {
     return this.http.post(`${this.URL}:${this.PORT}/convert/`, data);
   }
 
   healtCheck() {
-    return this.http.get(`${this.URL}:${this.PORT}`)
-      .subscribe(res => {
-        console.log(res)
-        return res;
-      });
+    return this.http.get(`${this.URL}:${this.PORT}`).subscribe((res) => {
+      console.log(res);
+      return res;
+    });
   }
 }
